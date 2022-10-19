a84550db49a21d66435d55176b5050a86611965c
Avinash Shukla <avinashukla2000@gmail.com>
2022-10-19 19:35:46 +0530

    EmailSection Validation

--- src/Screens/EmailSection/EmailSection.js
+++ src/Screens/EmailSection/EmailSection.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./EmailSection.css";
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
+
+const EmailSchema = Yup.object({
+  email: Yup.string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+});
 
 const EmailSection = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = ({ email }) => {
+    setIsSubmitted(true);
+  };
+
   return (
     <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
       <div className="container">
@@ -15,19 +29,46 @@ const EmailSection = () => {
             irrational matters of the heart.
           </div>
         </div>
+
         <div className="email-container">
-          <form>
-            <input placeholder="enter your email" />
-            <div className="input-arrow">
-              <img
-                src="https://frankdarling.com/assets/images/newsletter/arrow.svg"
-                alt="Submit-arrow"
-              />
-            </div>
-            <div className="helper-message">
-              Frank Darling doesn't spam, and neither should you.
-            </div>
-          </form>
+          <Formik
+            initialValues={{ email: "" }}
+            validationSchema={EmailSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ errors, values, touched, handleChange, handleBlur }) => {
+              console.log(errors, values, touched);
+
+              return (
+                <Form>
+                  <input
+                    id="email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                    name="email"
+                    placeholder="enter your email"
+                  />
+                  <button className="input-arrow" type="submit">
+                    <img
+                      src="https://frankdarling.com/assets/images/newsletter/arrow.svg"
+                      alt="Submit-arrow"
+                    />
+                  </button>
+
+                  {touched.email && Boolean(errors.email) ? (
+                    <div className="helper-message danger-text ">
+                      Sorry Darling, we need you to try that again.
+                    </div>
+                  ) : (
+                    <div className="helper-message">
+                      Frank Darling doesn't spam, and neither should you.
+                    </div>
+                  )}
+                </Form>
+              );
+            }}
+          </Formik>
         </div>
       </div>
     </div>
@@ -35,3 +76,8 @@ const EmailSection = () => {
 };
 
 export default EmailSection;
+
+// <div className="text-container text-center">
+//   <div>Confirmed</div>
+//   <div>Thank you Darling, we’ll be in touch.</div>
+// </div>
